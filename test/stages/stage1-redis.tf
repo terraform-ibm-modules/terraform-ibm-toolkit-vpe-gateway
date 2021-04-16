d3782e05be464cddb91a48c7e0d8ac488054e2ad
Sean Sundberg <seansund@us.ibm.com>
2021-04-16 17:59:30 -0500

    Updates test inputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-redis.tf
+++ test/stages/stage1-redis.tf
@@ -1,7 +1,8 @@
 module "redis" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-redis"
 
-  resource_group_name      = var.resource_group_name
-  resource_location        = var.region
-  name_prefix              = var.name_prefix
+  resource_group_name = var.resource_group_name
+  resource_location   = var.region
+  name_prefix         = var.name_prefix
+  ibmcloud_api_key    = var.ibmcloud_api_key
 }
