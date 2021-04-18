77f6445713fd82a9eb7049ddf1200bceae9cff66
Sean Sundberg <seansund@us.ibm.com>
2021-04-17 22:42:01 -0500

    Adds support for cos vpes
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ scripts/get-endpoint-target.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+CRN="$1"
+SERVICE="$2"
+REGION="$3"
+RESOURCE_GROUP="$4"
+OUTPUT_FILE="$5"
+
+if [[ -z "${IBMCLOUD_API_KEY}" ]]; then
+  echo "IBMCLOUD_API_KEY is required as an environment variable"
+  exit 1
+fi
+
+if [[ -z "${TMP_DIR}" ]]; then
+  TMP_DIR="./tmp"
+fi
+mkdir -p "${TMP_DIR}"
+
+echo "Logging into the ibmcloud account: region=${REGION}, resource_group=${RESOURCE_GROUP}"
+ibmcloud login --apikey "${IBMCLOUD_API_KEY}" -r "${REGION}" -g "${RESOURCE_GROUP}" || exit 1
+
+TMP_OUTPUT="${TMP_DIR}/endpoints.tmp"
+
+echo "Getting endpoint gateway targets"
+ibmcloud is endpoint-gateway-targets --output JSON > "${TMP_OUTPUT}"
+if [[ "${SERVICE}" == "cloud-object-storage" ]]; then
+  cat "${TMP_OUTPUT}" | \
+    jq --arg SERVICE "${SERVICE}" --arg REGION "${REGION}" '.[] | select(.crn) | select(.crn|test($SERVICE)) | select(.crn|test($REGION))' > "${OUTPUT_FILE}"
+else
+  cat "${TMP_OUTPUT}" | \
+    jq --arg CRN "${CRN}" '.[] | select(.crn) | select(.crn|test($CRN))' > "${OUTPUT_FILE}"
+fi
+
+if [[ -z "$(cat "${OUTPUT_FILE}")" ]]; then
+  echo "The output file is empty"
+  exit 1
+else
+  echo "Found matching resources"
+  cat "${TMP_OUTPUT}"
+fi
---
+++ test/stages/stage2-vpe-redis.tf
@@ -0,0 +1,14 @@
+module "vpe-redis" {
+  source = "./module"
+
+  resource_group_name = module.resource_group.name
+  region              = var.region
+  ibmcloud_api_key    = var.ibmcloud_api_key
+  name_prefix         = var.name_prefix
+  vpc_id              = module.vpc.id
+  vpc_subnets         = module.subnets.subnets
+  vpc_subnet_count    = module.subnets.count
+  resource_label      = "redis"
+  resource_crn        = module.redis.id
+  resource_service    = module.redis.service
+}
